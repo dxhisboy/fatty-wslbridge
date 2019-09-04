dd2ff58241c1d2acd7468d9fd09e67405d2ddb5f
Xiaohui Duan <dxhisboy@126.com>
2019-09-04 20:12:46 +0800

    moving

--- build.sh
+++ build.sh
@@ -1,28 +1,31 @@
 alias GIT="wsl.exe -e proxychains git"
-
+#set -x
 function update_source(){
     #export GIT=wsl.exe -e proxychains git
-    if [ -d $1 ]; then
+    if [ -d $1/.git ]; then
 	echo "Updating source of $1"
 	cd $1
-	GIT pull
+	GIT pull < /dev/null
 	cd ..
     else
+	if [ -d $1 ]; then
+	    rm $1 -rf
+	fi
 	echo "Cloning source of $1"
-	GIT clone $2
+	GIT clone $2 < /dev/null
     fi
 }
 function build_wslbridge2(){
     make -C wslbridge2/
-    wsl.exe -e make -C wslbridge2/
+    wsl.exe -e make -C wslbridge2/ < /dev/null
 }
 function build_fatty(){
     make -C fatty
 }
 function install_bin(){
-    if [ ! -d install/bin ]; then
+    if [ ! -d install/usr/bin ]; then
 	echo "Creating install directory"
-	mkdir -p install/bin
+	mkdir -p install/usr/bin
     fi
     echo "Installing wslbridge2"
     cp -v wslbridge2/bin/hvpty.exe \
@@ -30,14 +33,14 @@ function install_bin(){
        wslbridge2/bin/wslbridge2.exe \
        wslbridge2/bin/hvpty-backend \
        wslbridge2/bin/wslbridge2-backend \
-       install/bin
+       install/usr/bin
     echo "Installing fatty"
-    cp -v fatty/src/fatty.exe install/bin
+    cp -v fatty/bin/fatty.exe install/usr/bin
     echo "Installing fatty dependencies"
-    rm install/bin/*.dll
-    DEP=`ldd install/bin/fatty.exe | grep usr | awk '{print $3}'`
-    cp -v $DEP install/bin
-    cp -v /bin/cygwin-console-helper.exe install/bin
+    rm install/usr/bin/*.dll
+    DEP=`ldd install/usr/bin/*.exe | grep usr | awk '{print $3}' | sort | uniq`
+    cp -v $DEP install/usr/bin
+    cp -v /bin/cygwin-console-helper.exe install/usr/bin
     echo "Installing launch scripts"
     cp -v create_contextmenu.vbs \
        create_shortcut.vbs \
@@ -46,7 +49,8 @@ function install_bin(){
        startfattyat.bat \
        install
 }
-update_source wslbridge2 https://github.com/Biswa96/wslbridge2.git
-update_source fatty https://github.com/juho-p/fatty.git
+update_source wslbridge2 https://github.com/dxhisboy/wslbridge2.git
+update_source fatty https://github.com/dxhisboy/fatty.git
 build_wslbridge2
+build_fatty
 install_bin
--- create_shortcut.vbs
+++ create_shortcut.vbs
@@ -8,3 +8,14 @@ Set oLink = oWS.CreateShortcut(sLinkFile)
  '  oLink.WindowStyle = "1"   
  '  oLink.WorkingDirectory = "C:\Program Files\MyApp"
 oLink.Save
+
+scriptdir = CreateObject("Scripting.FileSystemObject").GetParentFolderName(WScript.ScriptFullName)
+Set oWS = WScript.CreateObject("WScript.Shell")
+sLinkFile = scriptdir + "\fatty-wsl2.lnk"
+Set oLink = oWS.CreateShortcut(sLinkFile)
+    oLink.TargetPath = scriptdir + "\bin\fatty.exe"
+    oLink.Arguments = "-w max -c " + scriptdir + "\mintty.conf " + scriptdir + "\bin\hvpty.exe -C ~"
+ '  oLink.Description = "Start wslbridge in fatty"   
+ '  oLink.WindowStyle = "1"   
+ '  oLink.WorkingDirectory = "C:\Program Files\MyApp"
+oLink.Save
